eb3638bcef0266c57841d774de9474480110bc21
raghavmour <113248575+raghavmour@users.noreply.github.com>
2025-05-18 21:32:46 +0530

    Update your_intelligence_module.py

--- your_intelligence_module.py
+++ your_intelligence_module.py
@@ -8,7 +8,7 @@ def analyze_user_tone(history):
     llm = ChatOpenAI(
         openai_api_key=st.secrets["GROQ_API_KEY"],
         temperature=0.0,
-        model_name="mistral-saba-24b",
+        model_name="meta-llama/llama-4-maverick-17b-128e-instruct",
         base_url="https://api.groq.com/openai/v1"
     )
 
